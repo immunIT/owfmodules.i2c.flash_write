2867d66fefa5e3fa4817877221a7f5e07a19da91
ghecko <ghecko78@gmail.com>
2020-06-30 15:21:24 +0200

    Adding git_init script

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.i2c.flash_write.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.i2c.flash_write.git
+git remote -v
