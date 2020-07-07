a9dc0a827ebf7016decfc888f54d96d3b389b8af
ghecko <ghecko78@gmail.com>
2020-07-07 11:43:35 +0200

    Update version

--- owfmodules/i2c/flash_write.py
+++ owfmodules/i2c/flash_write.py
@@ -17,7 +17,7 @@ class FlashWrite(AModule):
         super(FlashWrite, self).__init__(owf_config)
         self.meta.update({
             'name': 'I2C flash write',
-            'version': '1.0.0',
+            'version': '1.0.1',
             'description': 'Program generic I2C flash memories',
             'author': 'Jordan Ovrè / Ghecko <jovre@immunit.ch>, Paul Duncan / Eresse <pduncan@immunit.ch>'
         })
